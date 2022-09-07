01cf7b761e5e62622a77b777caed72225a138ea3
vimillan <20193tn066@utez.edu.mx>
2022-09-06 23:25:12 -0500

    feat: pagination added

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,65 @@
-const url = "https://pokeapi.co/api/v2/";
+const url = "https://pokeapi.co/api/v2/pokemon/";
+var listPage = [];
+var dataList = [];
 
-function getList() {
+function getList(urlData) {
   var itemList = document.querySelector("#itemList");
-  var dataList = [];
-  var item = {};
+  itemList.innerHTML = "";
 
-  fetch(url + "pokemon/")
+  if (urlData == "" || urlData == null) {
+    urlData = url;
+  }
+  
+  fetch(urlData)
     .then((response) => response.json())
     .then((data) => {
       dataList = data.results;
+      listPage = data;
       for (let data of dataList) {
         fetch(data.url)
           .then((response) => response.json())
           .then((data) => {
-            const img = data.sprites.other['official-artwork'].front_default;
-            console.log(img)
+            const img = data.sprites.other["official-artwork"].front_default;
+
+            var habilidades = `<ul class="list-group list-group-flush">`;
+            for (let hab of data.abilities) {
+              habilidades += `<li class="list-group-item">${hab.ability.name}</li>`;
+            }
+            habilidades += `</ul>`;
+
             itemList.innerHTML += `
-            <div class="col col-lg-4">
+            <div class="col-lg-4">
                 <div class="card mb-3">
                     <div class="row g-0">
-                        <div class="col-md-5">
+                        <div class="col-lg-5">
                             <img src="${img}" class="img-fluid rounded-start" alt="...">
                         </div>
-                        <div class="col-md-7">
+                        <div class="col-lg-7">
                             <div class="card-body">
                                 <h5 class="card-title">${data.name}</h5>
-                                <span class="badge text-bg-info">Habilidades</span>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">An item</li>
-                                </ul>
+                                <span class="badge text-bg-info">Habilidades: ${data.abilities.length}</span>
+                                <div>
+                                  ${habilidades}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            `
+            `;
           });
       }
     });
+}
+
+function getPaginationNext() {
+  if (listPage.next != null) {
+    getList(listPage.next);
+  }
+}
+
+function getPaginationPrev() {
+  if (listPage.previous != null) {
+    getList(listPage.previous);
+  }
 }
